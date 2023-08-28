a3e1da626af7528b84f2ae7d0ec5742499448214
nickdferrara <nick.d.ferrara@gmail.com>
2023-08-27 21:22:31 -0400

    Make logo and search bar

--- app/src/main/java/com/nickdferrara/ui_android_pokedexapp/MainActivity.kt
+++ app/src/main/java/com/nickdferrara/ui_android_pokedexapp/MainActivity.kt
@@ -3,20 +3,14 @@ package com.nickdferrara.ui_android_pokedexapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.nickdferrara.ui_android_pokedexapp.pokemonlist.PokemonListScreen
 import com.nickdferrara.ui_android_pokedexapp.ui.theme.UiandroidpokedexappTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = "pokemon_list_screen"
                 ){
                     composable(route = "pokemon_list_screen") {
-
+                        PokemonListScreen(navController = navController)
                     }
                     composable(
                         route = "pokemon_detail_screen/{dominantColor}/{pokemonName}",
